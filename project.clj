e63bcd7221e364e41a11219c884b37a6d169dd84
Dave Dixon <dave.dixon@allgress.com>
2014-11-22 08:37:55 -0800

    Testing :env

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
-                 [boot/core "2.0.0-pre24" :scope "provided"]
+                 [boot/core "2.0.0-pre26" :scope "provided"]
                  [tailrecursion/boot-useful "0.1.3" :scope "test"]]
   :plugins [[s3-wagon-private "1.1.2"]]
   :repositories [["s3" {:url           "s3p://aurora-repository/releases/"
