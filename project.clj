9604b6d5e05a82ee7dcddb6792f31c511a1bf9a7
Dave Dixon <dave.dixon@allgress.com>
2015-02-26 08:40:45 -0800

    Update for boot 2.0.0-rc10

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject allgress/boot-tasks "0.1.12"
+(defproject allgress/boot-tasks "0.1.13"
   :description "Allgress boot-tasks."
   :url "https://github.com/allgress/boot-tasks"
   :scm {:url "https://github.com/allgress/boot-tasks"}
   :license {:name "(c) 2015 Allgress Inc."
             :url "http://www.allgress.com"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
-                 [org.clojure/clojurescript "0.0-2727"]
+                 [org.clojure/clojurescript "0.0-2814"]
                  [leiningen-core "2.5.1"]
-                 [boot/core "2.0.0-rc9"]
-                 [adzerk/boot-cljs "0.0-2760-0"]
+                 [boot/core "2.0.0-rc10"]
+                 [adzerk/boot-cljs "0.0-2814-1"]
                  [adzerk/boot-reload "0.2.4"]
                  [deraen/boot-cljx "0.2.2"]
                  [org.clojure/data.json "0.2.5"]]
