7d756b102b9135b53ef18c25274e7c9f33f42b5b
Dave Dixon <dave.dixon@allgress.com>
2015-03-17 08:01:47 -0700

    Update dependencies. Version 0.1.15

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject allgress/boot-tasks "0.1.14"
+(defproject allgress/boot-tasks "0.1.15"
   :description "Allgress boot-tasks."
   :url "https://github.com/allgress/boot-tasks"
   :scm {:url "https://github.com/allgress/boot-tasks"}
@@ -7,12 +7,12 @@
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [org.clojure/clojurescript "0.0-2814"]
                  [leiningen-core "2.5.1"]
-                 [boot/core "2.0.0-rc10"]
+                 [boot/core "2.0.0-rc12"]
                  [adzerk/boot-cljs "0.0-2814-3"]
-                 [adzerk/boot-reload "0.2.4"]
+                 [adzerk/boot-reload "0.2.6"]
                  [deraen/boot-cljx "0.2.2"]
                  [cljsjs/boot-cljsjs "0.4.6"]
-                 [org.clojure/data.json "0.2.5"]]
+                 [org.clojure/data.json "0.2.6"]]
   :plugins [[s3-wagon-private "1.1.2"]]
   :repositories [["s3" {:url           "s3p://aurora-repository/releases/"
                         :username      :env/aws_key
